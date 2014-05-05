9c39a07d2fef4e7b840cc4ce86402807483314bb
Jason (Jayce^) Hall <jayce@lug-nut.com>
2014-05-05 15:27:07 -0600

    Stubbed readme docs in the perldoc

--- lib/Respite/Base.pm
+++ lib/Respite/Base.pm
@@ -333,5 +333,28 @@ sub __hello {
 }
 
 ###----------------------------------------------------------------###
+__END__
+
+=head1 RESPITE
+
+Respite Perl Server
+
+=head1
+
+Representational State (Perl Iscool) Transfer Engine
+
+=over 4
+
+=item DESCRIPTION
+
+a short period of rest or relief from something difficult or unpleasant. (notice the short period of rest)
+
+=item LICENSE
+
+Licensed under the same terms as Perl itself
+
+=back
+
+=cut
 
 1;
